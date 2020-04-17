290460bc64e593979d08bc120f9ee21def22be0e
Zach Sim <ZachS@datacom.co.nz>
2020-04-17 14:53:33 +1200

    Make landing root

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Landing from './landing/landing'
 import Home from './home/home'
 import { getSession } from '../lib/auth/auth'
 
+const SHOW_LANDING = true;
+
 const RootPage = (props) => {
   return (
     <>
-      {props.isAuthenticated
+      {(props.isAuthenticated && !SHOW_LANDING)
         ? <Home {...props} />
         : <Landing {...props} />}
     </>)
@@ -13,7 +15,7 @@ const RootPage = (props) => {
 
 RootPage.getInitialProps = async (ctx) => {
   const session = await getSession(ctx.req, ctx.store)
-  const page = session.isAuthenticated ? Home : Landing
+  const page = (session.isAuthenticated && !SHOW_LANDING) ? Home : Landing
   if (page.getInitialProps) {
     const pageProps = await page.getInitialProps(ctx)
     return pageProps
